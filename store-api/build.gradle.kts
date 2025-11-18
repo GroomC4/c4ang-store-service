92bfb833b5c9feb47f141d7fadc19f10d3f5478f
hayden-han <igluvjh@gmail.com>
2025-11-19 02:04:11 +0900

    feat: changing contract hub version to 1.0.0

--- store-api/build.gradle.kts
+++ store-api/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 // Platform Core 버전 관리
 val platformCoreVersion = "1.2.3"
 // Contract Hub 버전 관리
-val contractHubVersion = "1.0.0-SNAPSHOT"
+val contractHubVersion = "1.0.0"
 
 dependencies {
     // Kotlin
